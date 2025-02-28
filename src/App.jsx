53a102a2bc3b5033bef1b70eb46db63e9d9810cc
st-vignesh1 <vignesh@volopay.co>
2025-02-28 15:54:09 +0530

    created seperate file for constants and made the header bit flexible

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import { useState } from 'react';
+
 import RenderTable from './components/table/RenderTable';
+import { tableKeys } from './constants/constants';
 
 function App(){
-  const tableHeaders={
-    CUSTOMER:"customer",
-    CONTACT:"contact",
-    COUNTRY:"country",
+  
 
-  }
-  const [headers,setHeaders]=useState(Object.keys(tableHeaders).map(headers=>tableHeaders[headers]));
-  const [data,setData]=useState([
-    {customer:"Alfreds Futterkiste",contact:"Maria Anders",country:"Germany"},
-    {customer:"Centro comercial Moctezuma",contact:"Francisco Chang",country:"Mexico"},
-    {customer:"Ernst Handel",contact:"Roland Mendel",country:"Austria"},
-    {customer:"Island Trading",contact:"Helen Bennett",country:"UK"},
-    {customer:"Laughing Bacchus Winecellars",contact:"Yoshi Tannamuri",country:"Canada"},
-    {customer:"Italy",contact:"Giovanni Rovelli",country:"Italy"},
-  ]);
+  // const tableHeader = {
+  //   [tableHeaders.CUSTOMER]:{
+  //     columnLabel:"Customer service",
+  //     key:tableHeaders.CUSTOMER
+  //   }
+  // }
+  const tableHeaders={
+    [tableKeys.COMPANY]:{
+      columnLabel:"Company Name",
+      key:tableKeys.COMPANY
+    },
+    [tableKeys.CONTACT]:{
+      columnLabel:"Contact",
+      key:tableKeys.CONTACT
+    },
+    [tableKeys.COUNTRY]:{
+      columnLabel:"Country",
+      key:tableKeys.COUNTRY
+    }
+  } 
+
+
+  const headers=Object.keys(tableHeaders).map(header=>tableHeaders[header]);
+
+
+  const data=[
+    {company:"Alfreds Futterkiste",contact:"Maria Anders",country:"Germany"},
+    {company:"Centro comercial Moctezuma",contact:"Francisco Chang",country:"Mexico"},
+    {company:"Ernst Handel",contact:"Roland Mendel",country:"Austria"},
+    {company:"Island Trading",contact:"Helen Bennett",country:"UK"},
+    {company:"Laughing Bacchus Winecellars",contact:"Yoshi Tannamuri",country:"Canada"},
+    {company:"Italy",contact:"Giovanni Rovelli",country:"Italy"},
+  ];
 
         return <div className="p-0 m-0 box-border w-[100%] h-[100vh] flex items-center justify-center ">
           <RenderTable headers={headers} data={data}/>
---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const tableKeys={
+    COMPANY:"company",
+    CONTACT:"contact",
+    COUNTRY:"country",
+}
